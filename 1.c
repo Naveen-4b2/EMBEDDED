d5c1dd07dd5489328ea1c5ebab62d5c9e2e0c152
Naveen-4b2 <namburinaveen4b2@gmail.com>
2021-03-22 17:18:46 +0000

    add 1.c file

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	printf("EAT SLEEP WORK ");
+	printf("EAT SLEEP WORK ");
+	printf("EAT SLEEP WORK ");
+	printf("EAT SLEEP WORK ");
+	printf("EAT SLEEP WORK ");
+	printf("EAT SLEEP WORK ");
+	printf("EAT SLEEP WORK ");
+	printf("EAT SLEEP WORK ");
+	printf("EAT SLEEP WORK ");
+}
